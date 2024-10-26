53a026465d6bfe567b691b25968cf29b6f4d813a
Infinity <200540107113@darshan.ac.in>
2024-10-26 16:23:21 +0530

    Configured jest for testing & completed entity testcases

---
+++ tests/rateLimiter/entity/BurstRateLimiter.test.ts
@@ -0,0 +1,16 @@
+import { BurstRateLimiter } from '../../../src/rateLimiter/entity/BurstRateLimiter';
+import { RateLimitLevel } from '../../../src/rateLimiter/enums/RateLimitLevel';
+
+const burstRateLimiter: BurstRateLimiter = new BurstRateLimiter(10, RateLimitLevel.User, 100, 60);
+
+describe('isRequestAvailable function', () => {
+    test('Should return false when no requests available in burst capacity', () => {
+        expect(burstRateLimiter.isRequestAvailable(0)).toBeFalsy();
+    })
+})
+
+describe('isRequestAvailable function', () => {
+    test('Should return true when requests available in burst capacity', () => {
+        expect(burstRateLimiter.isRequestAvailable(1)).toBeTruthy();
+    })
+})
---
+++ tests/rateLimiter/entity/NonBurstRateLimiter.test.ts
@@ -0,0 +1,16 @@
+import { NonBurstRateLimiter } from "../../../src/rateLimiter/entity/NonBurstRateLimiter";
+import { RateLimitLevel } from "../../../src/rateLimiter/enums/RateLimitLevel";
+
+const nonBurstRateLimiter: NonBurstRateLimiter = new NonBurstRateLimiter(10, RateLimitLevel.User);
+
+describe('isRequestAvailable function', () => {
+    test('Should return false when already achived max requests per second', () => {
+        expect(nonBurstRateLimiter.isRequestAvailable(nonBurstRateLimiter.maxRequestPerSecond)).toBeFalsy();
+    })
+})
+
+describe('isRequestAvailable function', () => {
+    test('Should return true when not exceeded max request per second', () => {
+        expect(nonBurstRateLimiter.isRequestAvailable(9)).toBeTruthy();
+    })
+})
