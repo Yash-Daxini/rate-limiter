ad391abbcc478a3649306f6c257ebbfd8b7c35eb
Infinity <200540107113@darshan.ac.in>
2024-10-31 13:05:33 +0530

    Applied & considered priority configuration logic in the rate limiter

--- src/rateLimiter/entity/RatelimiterConfig.ts
+++ src/rateLimiter/entity/RatelimiterConfig.ts
@@ -2,13 +2,13 @@ import { RateLimitLevel } from "../enums/RateLimitLevel"
 import { RateLimitStrategy } from "../enums/RateLimitStrategy";
 
 export class RateLimiterConfig {
-    maxRequest: number;
+    maxRequestsPerSecond: number;
     rateLimitStrategy: RateLimitStrategy
     rateLimitLevel: RateLimitLevel;
     burstCapacity?: number | null;
     burstCapacityExpiryInSeconds?: number | null;
     constructor(maxRequest: number, rateLimitStrategy: RateLimitStrategy, rateLimiterLevel: RateLimitLevel, burstCapacity?: number, burstCapacityExpiryInSeconds?: number) {
-        this.maxRequest = maxRequest;
+        this.maxRequestsPerSecond = maxRequest;
         this.burstCapacity = burstCapacity;
         this.rateLimitStrategy = rateLimitStrategy
         this.rateLimitLevel = rateLimiterLevel;
--- src/rateLimiter/services/RateLimiterService.ts
+++ src/rateLimiter/services/RateLimiterService.ts
@@ -4,10 +4,14 @@ import { RateLimiterConfig } from "../entity/RatelimiterConfig";
 import { RateLimitStrategy } from "../enums/RateLimitStrategy";
 import { BurstRateLimiterService } from "./BurstRateLimiterService";
 import { NonBurstRateLimiterService } from "./NonBurstRateLimiterService";
+import priorityConfig from '../config/priorityConfig.json'
+import { RateLimitLevel } from "../enums/RateLimitLevel";
 
 export const RateLimiterService = async (config: RateLimiterConfig, key: string | undefined): Promise<boolean> => {
     if (!key)
         return false;
+
+    setPriorityConfig(config, key);
 
     if (config.rateLimitStrategy === RateLimitStrategy.BurstRateLimiter)
         return await handleBurstRateLimiter(config, key);
@@ -18,7 +22,7 @@ export const RateLimiterService = async (config: RateLimiterConfig, key: string 
 }
 
 const handleNonBurstRateLimiter = async (config: RateLimiterConfig, key: string): Promise<boolean> => {
-    const nonBurstRateLimiter = new NonBurstRateLimiter(config.maxRequest, config.rateLimitLevel);
+    const nonBurstRateLimiter = new NonBurstRateLimiter(config.maxRequestsPerSecond, config.rateLimitLevel);
 
     const redisKey = `rate_limit_nonBurst:${key}`;
 
@@ -27,9 +31,35 @@ const handleNonBurstRateLimiter = async (config: RateLimiterConfig, key: string)
 }
 
 const handleBurstRateLimiter = async (config: RateLimiterConfig, key: string): Promise<boolean> => {
-    const burstRateLimiter = new BurstRateLimiter(config.maxRequest, config.rateLimitLevel, config?.burstCapacity as number, config?.burstCapacityExpiryInSeconds as number);
+    const burstRateLimiter = new BurstRateLimiter(config.maxRequestsPerSecond, config.rateLimitLevel, config?.burstCapacity as number, config?.burstCapacityExpiryInSeconds as number);
 
     const redisKey = `rate_limit_burst:${key}`;
 
     return await new BurstRateLimiterService().canAcceptRequest(burstRateLimiter, redisKey);
 };
+
+const setPriorityConfig = (config: RateLimiterConfig, key: string): RateLimiterConfig => {
+    let specificPriorityConfig = getPriorityConfig(config, key);
+
+    if (specificPriorityConfig) {
+        config.maxRequestsPerSecond = specificPriorityConfig.maxRequestsPerSecond;
+        config.burstCapacity = specificPriorityConfig.burstCapacity;
+    }
+
+    return config;
+}
+
+const getPriorityConfig = (config: RateLimiterConfig, key: string): any => {
+    let specificPriorityConfig;
+    if (config.rateLimitLevel === RateLimitLevel.User) {
+        specificPriorityConfig = priorityConfig.User.filter(user => user.user === key)[0];
+    }
+    else if (config.rateLimitLevel === RateLimitLevel.IP) {
+        specificPriorityConfig = priorityConfig.IP.filter(ip => ip.ip === key)[0];
+    }
+    else if (config.rateLimitLevel === RateLimitLevel.Service) {
+        specificPriorityConfig = priorityConfig.Service.filter(service => service.service === key)[0];
+    }
+
+    return specificPriorityConfig;
+}
