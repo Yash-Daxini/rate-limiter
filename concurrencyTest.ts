758c0856274d506f12f7b8f58c6c59d59a737db4
Infinity <200540107113@darshan.ac.in>
2024-11-01 01:12:03 +0530

    Tested & recreated concurrency issue with redis & solved with lua script

---
+++ concurrencyTest.ts
@@ -0,0 +1,31 @@
+import RedisClient from "./src/rateLimiter/services/RedisService";
+
+let redis = RedisClient.getInstance();
+redis.set("testKey", 0, 'EX', 10);
+
+const luascript = `
+                    local key = KEYS[1]
+                    redis.call("INCR",key)
+                    return redis.call("GET",key)
+`
+
+setTimeout(async () => {
+    // operationWithoutLua(1);
+    operationWithLua(1);
+}, 1000);
+
+setTimeout(async () => {
+    // operationWithoutLua(2);
+    operationWithLua(2);
+}, 1000);
+
+let operationWithoutLua = async (operationCount: number) => {
+    console.warn(`Get operation before ${operationCount} :- ` + await redis.get("testKey"));
+    await redis.incr("testKey");
+    console.warn(`Get operation after ${operationCount} :- ` + await redis.get("testKey"));
+}
+
+let operationWithLua = async (operationCount: number) => {
+    let r = await redis.eval(luascript, 1, "testKey");
+    console.warn(`Get Operation ${operationCount} ` + r);
+}
