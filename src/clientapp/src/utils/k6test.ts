1d71b1703d174a7f12b0ee4a6c9c70296de9db3a
Infinity <200540107113@darshan.ac.in>
2024-11-10 23:07:13 +0530

    Implemented code to test rate limit using k6

--- src/clientapp/src/utils/k6test.ts
+++ src/clientapp/src/utils/k6test.ts
@@ -1,23 +1,20 @@
 import http from 'k6/http';
-import { check, sleep } from 'k6';
-// import { Rate } from 'k6/metrics';
+import { check } from 'k6';
 
 export const options = {
-    vus: 1,
-    duration: '1s'
-    // stages: [
-        // { duration: '1s', target: 200 },
-        // { duration: '10s', target: 200 },
-        // { duration: '10s', target: 200 },
-    // ]
+    executor: 'constant-arrival-rate',
+    timeUnit: '1s',
+    duration: '300s',
+
 }
 
 export default () => {
-    const params = { headers: { 'x-user-id': 'abc' } };
+    const params = { headers: { 'x-user-id': 'dd' } };
     const res = http.get("http://host.docker.internal:3000/api/service1/nonBurst", params);
-    check(res, { '200': (r) => r.status === 200,
-                 '429': (r) => r.status === 429 
+    check(res, {
+        '200': (r) => r.status === 200,
+        '429': (r) => r.status === 429
     })
-    // sleep(1);
+    // sleep(0.1);
 }
 
