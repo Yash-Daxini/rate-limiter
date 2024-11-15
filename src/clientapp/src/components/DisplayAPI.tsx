df04b2741e9b7b689c39fefb586ebb780fbbb816
Infinity <200540107113@darshan.ac.in>
2024-11-15 23:42:14 +0530

    Added urls in .env file & added .env.development

--- src/clientapp/src/components/DisplayAPI.tsx
+++ src/clientapp/src/components/DisplayAPI.tsx
@@ -1,19 +1,20 @@
 import '../styles/style.css';
 import Accordion from './Accordion';
+const env = import.meta.env;
 
 const DisplayAPI = () => {
     return (
         <div className='main'>
             <h4>Rate Limiter Test UI</h4>
             <Accordion key={1} title='User Level Rate Limiter (Service 1)'
-                url1='http://localhost:3000/api/service1/nonBurst'
-                url2='http://localhost:3000/api/service1/burst'
+                url1={env.VITE_SEVICE1_NONBURST_URL}
+                url2={env.VITE_SEVICE1_BURST_URL}
                 level='user' />
-            <Accordion key={2} title='IP Level Rate Limiter (Service 2)' url1='http://localhost:3001/api/service2/nonBurst'
-                url2='http://localhost:3001/api/service2/burst'
+            <Accordion key={2} title='IP Level Rate Limiter (Service 2)' url1={env.VITE_SEVICE2_NONBURST_URL}
+                url2={env.VITE_SEVICE2_BURST_URL}
                 level='ip' />
-            <Accordion key={3} title='Service Level Rate Limiter (Service 3)' url1='http://localhost:3000/api/service1/nonBurst/callservice3'
-                url2='http://localhost:3000/api/service1/burst/callservice3'
+            <Accordion key={3} title='Service Level Rate Limiter (Service 3)' url1={env.VITE_SEVICE3_NONBURST_URL}
+                url2={env.VITE_SEVICE3_BURST_URL}
                 level='service' />
         </div>
     )
